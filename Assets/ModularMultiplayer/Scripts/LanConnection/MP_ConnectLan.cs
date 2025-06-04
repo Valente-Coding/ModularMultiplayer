ea5f10e34502a30a765f72ede77765b50d06f4b6
Valente-Coding <goncalo.p.valente@hotmail.com>
2025-06-04 13:11:39 +0100

    School Connection Done plus UI

--- Assets/ModularMultiplayer/Scripts/LanConnection/MP_ConnectLan.cs
+++ Assets/ModularMultiplayer/Scripts/LanConnection/MP_ConnectLan.cs
@@ -1,14 +1,25 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using TMPro;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using UnityEngine;
 
 public class MP_ConnectLan : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI m_IPAddressText;
 
     void Start()
     {
+        var transport = GameObject.FindFirstObjectByType<UnityTransport>();
+        transport.ConnectionData.Address = GetAllLocalIPv4(NetworkInterfaceType.Ethernet).FirstOrDefault();
+
         NetworkManager.Singleton.OnServerStarted += () => {
             var transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+            m_IPAddressText.text = GetAllLocalIPv4(NetworkInterfaceType.Ethernet).FirstOrDefault();
             Debug.Log($"Server started successfully on {transport.ConnectionData.Address}:{transport.ConnectionData.Port}");
         };
     }
@@ -18,8 +29,10 @@ public class MP_ConnectLan : MonoBehaviour
         NetworkManager.Singleton.StartHost();
     }
 
-    public void StartClient()
+    public void StartClient(TextMeshProUGUI p_IPAddressInput)
     {
+        var transport = GameObject.FindFirstObjectByType<UnityTransport>();
+        transport.ConnectionData.Address = p_IPAddressInput.text.Trim((char)8203);;
         NetworkManager.Singleton.StartClient();
     }
 
@@ -31,5 +44,25 @@ public class MP_ConnectLan : MonoBehaviour
     public void StopAll()
     {
         NetworkManager.Singleton.Shutdown();
+    }
+
+
+    public static string[] GetAllLocalIPv4(NetworkInterfaceType _type)
+    {
+        List<string> ipAddrList = new List<string>();
+        foreach (NetworkInterface item in NetworkInterface.GetAllNetworkInterfaces())
+        {
+            if (item.NetworkInterfaceType == _type && item.OperationalStatus == OperationalStatus.Up)
+            {
+                foreach (UnicastIPAddressInformation ip in item.GetIPProperties().UnicastAddresses)
+                {
+                    if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        ipAddrList.Add(ip.Address.ToString());
+                    }
+                }
+            }
+        }
+        return ipAddrList.ToArray();
     }
 }
